70dfbc91bdcb6cdea420c889901deb84ef16132e
rawchor <ultimategingermaster@gmail.com>
2022-11-30 16:03:37 +0100

    added MP2

---
+++ MP2/js/valiCompForm.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    const firstNameInput = document.getElementById('firstName');
+    const lastNameInput = document.getElementById('lastName');
+    const dateOfBirth = document.getElementById('birthdate');
+    const dighy = document.getElementById('dighy');
+    const sailNumber = document.getElementById('sailNumber');
+
+    const errorFirstName = document.getElementById('errorFirstName');
+    const errorLastName = document.getElementById('errorLastName');
+    const errorDateOfBirth = document.getElementById('errorBirthdate');
+    const errorDighy = document.getElementById('errorDighy');
+    const errorSailNumber = document.getElementById('errorSailNumber');
+}
